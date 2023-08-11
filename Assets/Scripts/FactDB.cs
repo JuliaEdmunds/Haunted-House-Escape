8812d4e1726de6f92c44c87d7dd708349fad997e
JuliaEdmunds <julia.edmunds112@gmail.com>
2023-08-11 13:00:38 +0800

    Added fact database for keeping track of facts and managing inventory

---
+++ Assets/Scripts/FactDB.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+
+public static class FactDB
+{
+    private static Dictionary<string, int> m_Facts = new();
+
+    public static bool GetBoolFact(string key)
+    {
+        if (m_Facts.TryGetValue(key, out int value) && value != 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }       
+    }
+
+    public static int GetIntFact(string key)
+    {
+        if (m_Facts.TryGetValue(key, out int value))
+        {
+            return value;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public static void SetBoolFact(string key, bool isFullfilled)
+    {
+        m_Facts[key] = isFullfilled ? 1 : 0;
+    }
+
+    public static void SetIntFact(string key, EOperation operationType, int value)
+    {
+        switch (operationType)
+        {
+            default:
+            case EOperation.Add:
+                m_Facts[key] += value;
+                break;
+            case EOperation.Subtract:
+                m_Facts[key] -= value;
+                break;
+            case EOperation.Set:
+                m_Facts[key] = value;
+                break;
+        }
+    }
+}
